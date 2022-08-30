d23837a84d6bf9851838cd8a9efaa0ef2f2a5cd4
mishelrodri <rp18027@ues.edu.sv>
2022-08-30 09:12:47 -0600

    useSate

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,4 +1,4 @@
-import React, { useSyncExternalStore } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
@@ -6,16 +6,38 @@ import { Button } from "./Button";
 import { TaskCard } from "./Task";
 import { Saludar } from "./Saludar";
 import { Posts } from "./Posts";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
-  return <div>
-    
-  </div>
+  /*Devuelve un arreglo */
+  const [counter, setCounter] = useState(10);
+
+  return (
+    <div>
+      <h1>Counter:{counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >Sumar</button>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >Restar</button>
+      <button
+        onClick={() => {
+          setCounter(100);
+        }}
+      >Reiniciar
+      </button>
+    </div>
+  );
 }
 
 root.render(
   <>
-
+    <Counter />
   </>
 );
